2a23726cc81ea962b3704a4da6f73ff984cc9a55
Isabella Campos <isabellaszcamposs@gmail.com>
2023-04-28 19:34:27 -0300

    changing setup project

---
+++ src/main/java/com/microservice/journeytomars/JourneyToMarsApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.journeytomars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyToMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyToMarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/journeytomars/JourneyToMarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.journeytomars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyToMarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
